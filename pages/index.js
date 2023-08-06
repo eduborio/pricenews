a55b449d17d8ca3b58f3a886b2ffa0050389670b
Eduardo Borio <eduborio@gmail.com>
2023-08-06 00:31:36 +0000

    Iniciando Autenticação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1> Pricing and Revenue Management! </h1>;
+  return (
+    <h1>
+      <a href="https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URL">
+        Logar no ML
+      </a>
+    </h1>
+  );
 }
 
 export default Home;
